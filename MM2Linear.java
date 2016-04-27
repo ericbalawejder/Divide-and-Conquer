11a511e01154938e8d1df8649ae50892882841a0
Eric Balawejder <eric.balawejder@gmail.com>
2016-04-27 17:43:30 -0400

    fixed spacing again

--- MM2Linear.java
+++ MM2Linear.java
@@ -1,13 +1,16 @@
 package com.ebalawejder.MinMax2;
 
-public class MM2Linear {
-	public static void main(String[] args) {
+public class MM2Linear 
+{
+	public static void main(String[] args) 
+	{
 		// array of size 2^k, k a natural number, values 10..99
 
 		final int n = 64;
 		int[] arrayRandom = new int[n];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) 
+		{
 			// a <= x < a + b
 			arrayRandom[i] = 10 + (int) (Math.random() * 90);
 		}
@@ -16,12 +19,14 @@ public class MM2Linear {
 
 		// print the fourArray returned from minmax2Linear() method
 		int[] reference = minmax2Linear(arrayRandom);
-		for (int value : reference) {
+		for (int value : reference) 
+		{
 			System.out.print(value + " ");
 		}
 	}
 
-	private static int[] minmax2Linear(int[] array) {
+	private static int[] minmax2Linear(int[] array) 
+	{
 		int[] fourArray = new int[4];
 
 		int min1 = array[0];
@@ -29,16 +34,24 @@ public class MM2Linear {
 		int max2 = array[1];
 		int max1 = array[0];
 
-		for (int i = 1; i < array.length; i++) {
-			if (array[i] > max1) {
+		for (int i = 1; i < array.length; i++) 
+		{
+			if (array[i] > max1) 
+			{
 				max2 = max1;
 				max1 = array[i];
-			} else if (array[i] > max2) {
+			} 
+			else if (array[i] > max2) 
+			{
 				max2 = array[i];
-			} else if (array[i] < min1) {
+			} 
+			else if (array[i] < min1) 
+			{
 				min2 = min1;
 				min1 = array[i];
-			} else if (array[i] < min2) {
+			} 
+			else if (array[i] < min2) 
+			{
 				min2 = array[i];
 			}
 		}
@@ -51,9 +64,12 @@ public class MM2Linear {
 	}
 
 	// array print method with added number per line feature
-	public static void print(int[] array, int numberPerLine) {
-		for (int i = 0; i < array.length; i++) {
-			if (i != 0 && i % numberPerLine == 0) {
+	public static void print(int[] array, int numberPerLine) 
+	{
+		for (int i = 0; i < array.length; i++) 
+		{
+			if (i != 0 && i % numberPerLine == 0) 
+			{
 				System.out.println();
 			}
 			System.out.print(array[i] + " ");
