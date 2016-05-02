7cf49436e6c8538b2e8077362e603ea740631766
Eric Balawejder <eric.balawejder@gmail.com>
2016-05-02 17:49:58 -0400

    created merge method

--- MM2DaC.java
+++ MM2DaC.java
@@ -4,13 +4,79 @@ public class MM2DaC
 {
 	public static void main(String[] args) 
 	{
-		int[] sample = {15,11,36,13};
-		Arrays.print(minmax2Four(sample));
+		int[] sample1 = {15,111,36,2};
+		int[] sample2 = {200,1,6,23};
+		Arrays.print(minmax2Four(sample1));
+		Arrays.print(minmax2Four(sample2));
+		
+		Arrays.print(merge(sample1, sample2));
 	}
 
 	private static int[] minmax2DaC(int[] array)
 	{
 		int[] fourArray = new int[4];
+		return fourArray;
+	}
+	
+	private static int[] merge(int[] array1, int[] array2)
+	{
+		int[] fourArray = new int[4];
+		
+		int[] fourArray1 = minmax2Four(array1);
+		int[] fourArray2 = minmax2Four(array2);
+		
+		// smallest two elements from both four arrays
+		if (fourArray1[0] < fourArray2[0])
+		{
+			fourArray[0] = fourArray1[0];
+			if (fourArray2[0] < fourArray1[1])
+			{
+				fourArray[1] = fourArray2[0];
+			}
+			else 
+			{
+				fourArray[1] = fourArray1[1];
+			}
+		}
+		else
+		{
+			fourArray[0] = fourArray2[0];
+			if (fourArray1[0] < fourArray2[1])
+			{
+				fourArray[1] = fourArray1[0];
+			}
+			else
+			{
+				fourArray[1] = fourArray2[1];
+			}
+		}
+		
+		// largest two elements from both four arrays
+		if (fourArray1[3] > fourArray2[3])
+		{
+			fourArray[3] = fourArray1[3];
+			if (fourArray2[3] > fourArray1[2])
+			{
+				fourArray[2] = fourArray2[3];
+			}
+			else
+			{
+				fourArray[2] = fourArray1[2];
+			}
+		}
+		else
+		{
+			fourArray[3] = fourArray2[3];
+			if (fourArray1[3] > fourArray2[2])
+			{
+				fourArray[2] = fourArray1[3];
+			}
+			else
+			{
+				fourArray[2] = fourArray2[2];
+			}
+		}
+		
 		return fourArray;
 	}
 	
@@ -25,6 +91,7 @@ public class MM2DaC
 		int c = array[2];
 		int d = array[3];
 		
+		// compare first two elements
 		if (a < b)
 		{
 			min1 = a;
@@ -35,6 +102,7 @@ public class MM2DaC
 			min1 = b;
 			min2 = a;
 		}
+		// compare second two elements
 		if (c < d)
 		{
 			max2 = c;
@@ -45,6 +113,7 @@ public class MM2DaC
 			max2 = d;
 			max1 = c;
 		}
+		// compare first and third element
 		if (min1 < max2)
 		{
 			a = min1;
@@ -55,6 +124,7 @@ public class MM2DaC
 			a = max2;
 			c = min1;
 		}
+		// compare second and fourth element
 		if (min2 < max1)
 		{
 			b = min2;
@@ -65,8 +135,10 @@ public class MM2DaC
 			b = max1;
 			d = min2;
 		}
+		// compare second and third element
 		if (b > c)
 		{
+			// swap b and c
 			int temp = c;
 			c = b;
 			b = temp;
